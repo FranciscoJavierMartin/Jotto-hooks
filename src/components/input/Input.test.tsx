ed3ab1e0477c921e2ed5472ddd31cf37c4d84d9d
Francisco <javiermartincuenca@gmail.com>
2019-10-18 23:35:18 +0200

    Mock useState in test

--- src/components/input/Input.test.tsx
+++ src/components/input/Input.test.tsx
@@ -5,14 +5,32 @@ import Input from './Input';
 
 /**
  * Setup function for app component
+ * @param {string} secreteWord - Word to guess 
  * @returns { ShallowWrapper }
  */
-const setup = (): ShallowWrapper => {
-  return shallow(<Input/>);
+const setup = (secretWord:string = 'party'): ShallowWrapper => {
+  return shallow(<Input secretWord={secretWord}/>);
 }
 
 test('Input renders without error', () => {
   const wrapper: ShallowWrapper = setup();
   const inputComponent = findByTestAttr(wrapper, 'component-input');
   expect(inputComponent.length).toBe(1);
+});
+
+describe('state controlled input field', () => {
+
+  test('state updates with value of input box upon change', () => {
+    const mockSetCurrentGuess: jest.Mock<any, any> = jest.fn();
+    React.useState = jest.fn(() => ['', mockSetCurrentGuess]);
+
+    const wrapper = setup();
+    const inputBox = findByTestAttr(wrapper, 'input-box');
+
+    const inputWord = 'train';
+    const mockEvent = { target: { value: inputWord } };
+    inputBox.simulate('change', mockEvent);
+
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith(inputWord);
+  });
 });
--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
 
 interface IInputProps {
-
+  secretWord: string;
 }
 
 const input = (props: IInputProps) => {
+  const [ currentGuess, setCurrentGuess ] = React.useState<string>('');
   return (
     <div data-test="component-input">
-      
+      <form className="form-inline">
+        <input
+          data-test="input-box"
+          className="mb-2 mx-sm-3"
+          type="text"
+          placeholder="enter guess"
+          value={currentGuess}
+          onChange={(event) => setCurrentGuess(event.target.value)}/>
+        <button
+          data-test="submit-button"
+          className="btn btn-primary mb-2">
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
