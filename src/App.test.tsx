1c54039cdd42eda31f29783a7dcab72d14856373
Francisco <javiermartincuenca@gmail.com>
2019-10-19 16:34:36 +0200

    Language setup

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,18 +1,86 @@
 import React from 'react';
-import { shallow, ShallowWrapper } from 'enzyme';
+import { ReactWrapper, mount } from 'enzyme';
 import { findByTestAttr } from '../test/testUitls'
 import App from './App';
 
+import hookActions from './actions/hookActions';
+
+const mockGetSecretWord = jest.fn();
+
 /**
  * Setup function for app component. 
- * @returns { ShallowWrapper }
+ * @param {string} secretWord - desired secretWord state value for test
+ * @returns { ReactWrapper }
  */
-const setup = (): ShallowWrapper => {
-  return shallow(<App/>);
+const setup = (secretWord: string = 'party'): ReactWrapper => {
+  mockGetSecretWord.mockClear();
+  hookActions.getSecretWord = mockGetSecretWord;
+
+  const mockUseReducer = jest.fn()
+    .mockReturnValue([
+      { secretWord },
+      jest.fn()
+    ]);
+
+  React.useReducer = mockUseReducer;
+
+  return mount(<App/>);
 }
 
-test('App renders without error', () => {
+xit('App renders without error', () => {
   const wrapper = setup();
   const component = findByTestAttr(wrapper, 'component-app');
   expect(component.length).toBe(1);
+});
+
+describe('getSecretWord calls', () => {
+  
+  test('getSecretWord gets called on App mount', () => {
+    setup();
+    expect(mockGetSecretWord).toHaveBeenCalled();
+  });
+
+  test('secretWord does not update on App update', () => {
+    const wrapper = setup();
+    mockGetSecretWord.mockClear();
+    wrapper.setProps();
+
+    expect(mockGetSecretWord).not.toHaveBeenCalled();
+  });
+});
+
+describe('secretWord is not null', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = setup('party');
+  });
+
+  test('renders app when secretWord is not null', () => {
+    const appComponent = findByTestAttr(wrapper, 'component-app');
+    expect(appComponent.exists()).toBe(true);
+  });
+
+  test('does not render sinner when secretWord is not null', () => {
+    const spinnerComponent = findByTestAttr(wrapper, 'spinner');
+    expect(spinnerComponent.exists()).toBe(false);
+  });
+});
+
+describe('secretWord is null', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = setup(null);
+  });
+
+  test('does not render app when secretWord is null', () => {
+    const appComponent = findByTestAttr(wrapper, 'component-app');
+    expect(appComponent.exists()).toBe(false);
+  });
+
+  test('renders sinner when secretWord is null', () => {
+    const spinnerComponent = findByTestAttr(wrapper, 'spinner');
+    expect(spinnerComponent.exists()).toBe(true);
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,61 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Congrats from './components/congrats/Congrats';
 import GuessedWords from './components/guessedWords/GuessedWords';
+import Input from './components/input/Input';
+import hookActions from './actions/hookActions';
 
-class App extends Component<{},{}> {
+/**
+ * reducer to update state
+ * @param { object } state - existing state
+ * @param { object } action - contains 'type' and 'payload' properties for the 
+ *                            state update for example: 
+ *                            { type: "setSecretWord", payload: "party" }
+ * @returns { object } - new state
+ */
+function reducer(state, action){
+  let newState;
 
-  render(){
-  return (
-    <div data-test="component-app">
-      
-    </div>
-  );}
+  switch(action.type){
+    case 'setSecretWord':
+      newState = { ...state, secretWord: action.payload }
+      break;
+    default:
+      newState = { ...state };
+      throw new Error(`Invalid action type: ${action.type}`)
+  }
+
+  return newState;
+}
+const App = () => {
+  const [state, dispatch] = React.useReducer(
+    reducer,
+    { secretWord: null }
+  );
+
+  const setSecretWord = (secretWord: string) => dispatch({
+    type: 'setSecretWord',
+    payload: secretWord
+  });
+
+  React.useEffect(() => {
+    hookActions.getSecretWord(setSecretWord)
+  }, []);
+
+  let content = !state.secretWord ?
+    (
+      <div className="countainer" data-test="spinner">
+        <div className="spinner-border" role="status">
+          <span className="sr-only">Loading...</span>
+        </div>
+        <p>Loading secret word</p>
+      </div>
+    ) : (
+      <div className="container" data-test="component-app">
+        <Input secretWord={state.secretWord}/>
+      </div>
+    );
+
+  return content;
 }
 
 export default App;
