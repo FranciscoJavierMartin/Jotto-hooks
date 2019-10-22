e97ddac4513c7a98ea4e3db2e233a29712f38b4a
Francisco <javiermartincuenca@gmail.com>
2019-10-22 18:07:19 +0200

    Docs added

---
+++ docs/precache-manifest.c85a1e407dd350377cdb90599f19c7a0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a23a0680d0716a7d171cd898d4cf722a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "958588ca5d2fa0aca414",
+    "url": "/static/js/2.78c12721.chunk.js"
+  },
+  {
+    "revision": "c4dad0c179f335b55bd8",
+    "url": "/static/js/main.7614d284.chunk.js"
+  },
+  {
+    "revision": "624de4fa19340d7de5bc",
+    "url": "/static/js/runtime-main.b30d36cf.js"
+  }
+]);
